7de9894bb9d25e42ea41c93750ef1cdde4a5233c
Thomas Liebert <thomas.d.liebert@gmail.com>
2021-01-12 13:47:07 -0600

    reorganization via class

--- scripts.js
+++ scripts.js
@@ -8,96 +8,118 @@ let myLibrary = [demobook, demobook2];
 
 // Book constructor
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read;
-    }
+class Book {
 
-// prototype method additions to Book constructor
-
-Book.prototype.info = function () {
-    return `${this.title} by ${this.author}`;
-}
-
-Book.prototype.readstatustoggle = function () {
-    this.read = !this.read;
-    makeLibrary();
-}
-
-Book.prototype.removeBookFromLibrary = function () {
-    console.log(this.libIndex);
-    myLibrary = myLibrary.filter(book => book.libIndex !== this.libIndex)
-    makeLibrary();
-}
-
-Book.prototype.createCard = function() {
-    let bookcard = document.createElement("div");
-    bookcard.setAttribute("class", "bookcard");
-
-    let bookinfo = document.createElement("ul");
-    bookcard.classList.add("singleBookCard");
-
-    let readbutton = document.createElement("button")
-    readbutton.textContent = "Read it?"
-    readbutton.addEventListener("click", this.readstatustoggle.bind(this));
-
-    let deletebutton = document.createElement("button");
-    deletebutton.textContent = "Remove book from library";
-    deletebutton.addEventListener("click", this.removeBookFromLibrary.bind(this));
-
-    for (let key of Object.keys(this)) {
-        let listitem = document.createElement("li");
-        listitem.setAttribute("class", "bookListItem")
-        switch (key) {
-            case "read": if (this[key]) {
-                listitem.textContent = "Read it!";
-            }          else {
-                listitem.textContent = "Need to read!";
-            }
-            break; 
-            case "title":
-            case "author":
-            case "pages":
-                listitem.textContent = `${key}: ${this[key]}`;
-                break;
-            default: break;
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read;
         }
-        bookinfo.appendChild(listitem);
+
+    info() {
+        return `${this.title} by ${this.author}`;
     }
-    bookcard.appendChild(bookinfo);
-    bookcard.appendChild(readbutton);
-    bookcard.appendChild(deletebutton);
-    libraryDisplay.appendChild(bookcard);
+
+    readstatustoggle() {
+        this.read = !this.read;
+    }
+
+    createCard() {
+        let bookcard = document.createElement("div");
+        bookcard.setAttribute("class", "bookcard");
+    
+        let bookinfo = document.createElement("ul");
+        bookcard.classList.add("singleBookCard");
+    
+        let readbutton = document.createElement("button")
+        readbutton.textContent = "Read it?"
+        readbutton.addEventListener("click", this.readstatustoggle.bind(this));
+    
+        let deletebutton = document.createElement("button");
+        deletebutton.textContent = "Remove book from library";
+        deletebutton.addEventListener("click", removeBookFromLibrary.bind(this));
+    
+        for (let key of Object.keys(this)) {
+            let listitem = document.createElement("li");
+            listitem.setAttribute("class", "bookListItem")
+            switch (key) {
+                case "read": if (this[key]) {
+                    listitem.textContent = "Read it!";
+                }          else {
+                    listitem.textContent = "Need to read!";
+                }
+                break; 
+                case "title":
+                case "author":
+                case "pages":
+                    listitem.textContent = `${key}: ${this[key]}`;
+                    break;
+                default: break;
+            }
+            bookinfo.appendChild(listitem);
+        }
+        bookcard.appendChild(bookinfo);
+        bookcard.appendChild(readbutton);
+        bookcard.appendChild(deletebutton);
+        libraryDisplay.appendChild(bookcard);
+    }
 }
 
-// add a single instance of a book to the library 
+const bookLibrary = (function(){
+    let removeBookFromLibrary = function () {
+        console.log(this)
+        console.log(this.libIndex);
+        myLibrary = myLibrary.filter(book => book.libIndex !== this.libIndex)
+        makeLibrary();
+    }
+    
+    function addBookToLibrary(title, author, pages, read) {
+        myLibrary.push(new Book(title, author, pages, read));
+    }
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
-}
+    function prepareLibrary() {
+    }
 
+    function removeBook() {
 
-// fetches the submitBook button and runs submitBookForm when it is clicked. submitBookForm 
-// gathers values currently in the various input fields and passes them as arguments to 
-// the addBookToLibrary function.  
+    }
 
-const submitButton = document.getElementById("submitBook");
-submitButton.addEventListener("click", submitBookForm);
+    return {
+        prepareLibrary,
+        removeBook
+    }
+})()
 
-//Add book button that brings up the submit form 
+const renderer = (function() {
+    //Add book button that brings up the submit form 
 
-const addBookButton = document.getElementById("showBookForm");
-addBookButton.addEventListener("click", toggleForm);
+    const addBookButton = document.getElementById("showBookForm");
+    addBookButton.addEventListener("click", toggleForm);
 
-const submitForm = document.getElementById("submitform");
+    const submitForm = document.getElementById("submitform");
 
-function toggleForm() {
-    submitForm.classList.toggle("showForm")
-}
+    function toggleForm() {
+        submitForm.classList.toggle("showForm")
 
-// take the current inputs run them through the addBookToLibrary
+        //
+
+    const submitButton = document.getElementById("submitBook");
+    submitButton.addEventListener("click", submitBookForm);
+
+        // take an individual book object and append it to the library div 
+
+    const libraryDisplay = document.getElementById("libDisplay")
+
+    function displayBookInLibrary (book, indexInLibrary) {
+        book.libIndex = indexInLibrary;
+        book.createCard();
+        }
+
+    
+
+const mainController = (function(){
+    // take the current inputs run them through the addBookToLibrary
 
 function submitBookForm () {
     let tf = document.getElementById("title").value;
@@ -110,19 +132,15 @@ function submitBookForm () {
     makeLibrary();
 }
 
+
+}
+})()
+
 function resetForm () {
     let allInputs = document.querySelectorAll("input");
     allInputs.forEach(input => input.value = "");
 }
 
-// take an individual book object and append it to the library div 
-
-const libraryDisplay = document.getElementById("libDisplay")
-
-function displayBookInLibrary (book, indexInLibrary) {
-    book.libIndex = indexInLibrary;
-    book.createCard();
-    }
 
 // iterate over the library array and add a book to the library div for each
 
